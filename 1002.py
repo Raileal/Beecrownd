beca856f67dd35475e61ba00304ed97116f5a08e
Raileal <115902100+Raileal@users.noreply.github.com>
2023-03-18 16:57:45 -0300

    Create 1002.py

---
+++ 1002.py
@@ -0,0 +1,7 @@
+#area =n*(raio*raio)
+n = 3.14159
+Raio = float(input())#entra com o valor float
+
+Area = n * Raio**2#Calcula a area (Raio**2) é raio elevado ao quadrado
+
+print('A=%.4f' %Area)#printa
