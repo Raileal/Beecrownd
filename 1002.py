4b7cead5a6ecff756499bdaa60e2b20f691636fe
Raileal <115902100+Raileal@users.noreply.github.com>
2023-03-23 16:09:54 -0300

    Delete 1002.py

--- 1002.py
+++
@@ -1,7 +0,0 @@
-#area =n*(raio*raio)
-n = 3.14159
-Raio = float(input())#entra com o valor float
-
-Area = n * Raio**2#Calcula a area (Raio**2) é raio elevado ao quadrado
-
-print('A=%.4f' %Area)#printa
