93ecfc6a5c3d2f8b66b66a69aae65ef5f813db52
Raileal <115902100+Raileal@users.noreply.github.com>
2023-03-24 09:18:48 -0300

    Delete 1008.py

--- 1008.py
+++
@@ -1,9 +0,0 @@
-A = int(input())
-B = int(input())
-C = float(input())
-
-Number = A
-salary = B*C
-
-print(f"NUMBER = {Number}")
-print(f"SALARY = U$ {salary:.2f}")#Para pegar numero de casas decimais.
